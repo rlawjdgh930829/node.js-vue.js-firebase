4df3d355ec1346fe7067676ca1de5de779a422da
kimjeongho <kimjeongho930829@gmail.com>
2021-06-04 22:15:48 +0900

    파이어베이스 연동

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,17 @@
 <template>
   <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
+    <v-app-bar app color="primary" dark>
       <v-app-bar-nav-icon @click="drawer = !drawer"/>
-      <site-title :title="title"></site-title>
+      <site-title :title="site.title"></site-title>
       <v-spacer/>
     </v-app-bar>
     <v-navigation-drawer app v-model="drawer">
-      <site-menu></site-menu>
+      <site-menu :items="site.menu"></site-menu>
     </v-navigation-drawer>
-    <v-content>
+    <v-main>
       <router-view/>
-    </v-content>
-    <site-footer :footer="footer"></site-footer>
+    </v-main>
+    <site-footer :footer="site.footer"></site-footer>
   </v-app>
 </template>
 
@@ -30,9 +26,54 @@ export default {
   data () {
     return {
       drawer: false,
-      item: [],
-      title: '나의 타이틀',
-      footer: '푸터입니다'
+      site: {
+        menu: [
+          {
+            title: 'home',
+            icon: 'mdi-home',
+            subItems: [
+              {
+                title: 'Dashboard',
+                to: '/'
+              },
+              {
+                title: 'About',
+                to: '/about'
+              }
+            ]
+          },
+          {
+            title: 'about',
+            active: true,
+            icon: 'mdi-account',
+            subItems: [
+              {
+                title: 'xxx',
+                to: '/xxx'
+              }
+            ]
+          }
+        ],
+        title: '나의 타이틀',
+        footer: '푸터입니다'
+      }
+    }
+  },
+  created () {
+    this.subscribe()
+  },
+  methods: {
+    subscribe () {
+      this.$firebase.database().ref().child('site').on('value', (sn) => {
+        const v = sn.val()
+        if (!v) {
+          this.$firebase.database().ref().child('site').set(this.site)
+          return
+        }
+        this.site = v
+      }, (e) => {
+        console.log(e.message)
+      })
     }
   }
 }
--- src/views/site/menu.vue
+++ src/views/site/menu.vue
@@ -41,36 +41,10 @@
 
 <script>
 export default {
-  props: ['footer'],
+  props: ['items'],
   data () {
     return {
-      items: [
-        {
-          title: 'home',
-          icon: 'mdi-home',
-          subItems: [
-            {
-              title: 'Dashboard',
-              to: '/'
-            },
-            {
-              title: 'About',
-              to: '/about'
-            }
-          ]
-        },
-        {
-          title: 'about',
-          active: true,
-          icon: 'mdi-account',
-          subItems: [
-            {
-              title: 'xxx',
-              to: '/xxx'
-            }
-          ]
-        }
-      ]
+
     }
   }
 }
