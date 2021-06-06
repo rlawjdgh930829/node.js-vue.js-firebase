bc9fd900d216c0f396199dd3c468701b72340867
KimJeongho <kimjeongho@archive-asia.co.jp>
2021-06-06 18:52:17 +0900

    로그인 정보 보기

--- src/views/site/sign.vue
+++ src/views/site/sign.vue
@@ -1,5 +1,6 @@
 <template>
-    <v-menu offset-y>
+    <v-progress-circular indeterminate v-if="loading"></v-progress-circular>
+    <v-menu offset-y v-else-if="!$store.state.fireUser">
         <template v-slot:activator="{ on }">
             <v-btn icon v-on="on"><v-icon>mdi-account</v-icon></v-btn>
         </template>
@@ -9,6 +10,18 @@
             <v-card-actions>
                 <v-btn color="red" dark @click="signInWithGoogle" block><v-icon>mdi-google</v-icon> 구글로 로그인</v-btn>
             </v-card-actions>
+        </v-card>
+    </v-menu>
+    <v-menu offset-y v-else>
+        <template v-slot:activator="{ on }">
+            <v-btn icon v-on="on">
+                <v-avatar size="32">
+                    <v-img :src=$store.state.fireUser.photoURL></v-img>
+                </v-avatar>
+            </v-btn>
+        </template>
+        <v-card>
+            <v-card-title>정보</v-card-title>
             <v-card-actions>
                 <v-btn color="" dark @click="signOut" block>로그아웃</v-btn>
             </v-card-actions>
@@ -29,7 +42,7 @@ export default {
       this.loading = true
       try {
         const sn = await this.$firebase.auth().signInWithPopup(provider)
-        console.log(sn.user)
+        this.$store.commit('setFireUser', sn.user)
       } finally {
         this.loading = false
       }
