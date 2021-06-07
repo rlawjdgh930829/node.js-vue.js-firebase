0b859e64cdd8856492d6eca1689662e7f8323252
KimJeongho <kimjeongho@archive-asia.co.jp>
2021-06-08 00:42:03 +0900

    파일 업로드 테스트

--- src/router/index.js
+++ src/router/index.js
@@ -22,6 +22,11 @@ const routes = [
     path: '/board',
     name: 'board',
     component: () => import('../views/board/')
+  },
+  {
+    path: '/storage',
+    name: 'storage',
+    component: () => import('../views/storage')
   }
 ]
 
